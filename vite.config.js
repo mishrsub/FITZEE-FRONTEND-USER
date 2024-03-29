2683b5f416e14c65007e54a205439c97219eb3ab
mishrsub <mishrsub@gmail.com>
2024-03-29 13:21:06 -0700

    assign port 80

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  chunkSizeWarningLimit: 1600,
-  server: {
-    port: 80 // Set the desired port number here
-  }
-})
+    plugins: [react()],
+    chunkSizeWarningLimit: 1600,
+    server: {
+        port: 80,
+        host: "0.0.0.0", // Listen on all network interfaces
+    },
+});
