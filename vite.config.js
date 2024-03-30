5cccf44c362b8f3f8fed3281af4e5e5308d64bd7
mishrsub <mishrsub@gmail.com>
2024-03-30 12:07:31 +0530

    removing /fiitjee_mumbai-v11 from home page url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   chunkSizeWarningLimit: 1600,
-  server: {
-    port: 80 // Set the desired port number here
-  }
+  // server:{
+  //   port:80
+  // }
 })
+
+
+
