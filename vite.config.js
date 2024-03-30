2d8a7941eae8b4c80f3d62df2a0e471d6f5eba26
mishrsub <mishrsub@gmail.com>
2024-03-29 19:40:11 -0700

    assign port 80

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
     server: {
         port: 80,
-        host: "35.154.95.255", // Listen on all network interfaces
+        host: "myfiitjee.com", // Listen on all network interfaces
+        strictPort: true,
     },
 });
